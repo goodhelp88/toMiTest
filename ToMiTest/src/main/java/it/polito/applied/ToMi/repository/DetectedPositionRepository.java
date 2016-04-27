eeb5c4fc8b0b49c89e2ed00bbf13f0b943e805fc
andreabuonaiuto <andreabuonaiuto@192.168.1.21>
2016-04-27 13:21:49 +0200

    ok

--- ToMiTest/src/main/java/it/polito/applied/ToMi/repository/DetectedPositionRepository.java
+++ ToMiTest/src/main/java/it/polito/applied/ToMi/repository/DetectedPositionRepository.java
@@ -1,11 +1,9 @@
 package it.polito.applied.ToMi.repository;
 
-import java.util.List;
-
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 import it.polito.applied.ToMi.pojo.DetectedPosition;
 
-public interface DetectedPositionRepository extends MongoRepository<DetectedPosition, String>{
+public interface DetectedPositionRepository extends MongoRepository<DetectedPosition, String>, CustomDetectedPositionRepository{
 
 }
--- ToMiTest/src/main/java/it/polito/applied/ToMi/service/AppServiceImpl.java
+++ ToMiTest/src/main/java/it/polito/applied/ToMi/service/AppServiceImpl.java
@@ -17,4 +17,6 @@ public class AppServiceImpl implements AppService{
 		posRepo.insert(position);
 	}
 
+	
+
 }
